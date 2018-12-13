edfd2f3b53e67dcea71d9e434788d43c36b7fac9
Mason Gup <mgup@mdsol.com>
2018-12-13 17:32:05 -0500

    Initial commit - basic functionality

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::*;
+use serde_derive::Deserialize;
+use rocket_contrib::json::Json;
+use log::{info};
+
+#[derive(Deserialize)]
+struct UrlVerify {
+    token: String,
+    challenge: String,
+    #[serde(rename = "type")]
+    request_type: String,
+}
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct Message {
+    #[serde(rename = "type")]
+    request_type: String,
+    channel: String,
+    user: String,
+    text: String,
+    ts: String,
+    edited: Option<MessageEdit>,
+}
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct MessageEdit {
+    user: String,
+    ts: String,
+}
+
+#[post("/event", data = "<challenge>", rank = 2)]
+fn challenge_receive(challenge: Json<UrlVerify>) -> String {
+    info!("Received url verify request with token {} and type {}", &challenge.token, &challenge.request_type);
+    challenge.challenge.clone()
+}
+
+#[post("/event", data = "<message>")]
+fn message_receive(message: Json<Message>) {
+    info!("Received message with text '{}' by user '{}'", &message.text, &message.user);
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![challenge_receive, message_receive])
+        .launch();
+}
