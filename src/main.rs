1b3b332f76e776dca041803f3981293f9f8a5ac9
Mason Gup <mgup@mdsol.com>
2018-12-18 11:30:30 -0500

    Add in a bunch of error logging

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,37 @@ mod test;
 
 #[post("/event", data = "<message>")]
 fn message_receive(message: Json<Value>, slack_params: State<SlackParams>) -> Result<Json<Value>, Status> {
-    if let Value::Object(message_map) = message.into_inner() {
-        let token_maybe = message_map.get("token").and_then(|token_val| token_val.as_str());
-        if token_maybe.is_none() || token_maybe.unwrap() != slack_params.verification_token {
-            info!("Got a bad or empty verification token");
-            return Err(Status::BadRequest);
+    match message.into_inner() {
+        Value::Object(message_map) => {
+            let token_maybe = message_map.get("token").and_then(|token_val| token_val.as_str());
+            if token_maybe.is_none() || token_maybe.unwrap() != slack_params.verification_token {
+                info!("Got a bad or empty verification token");
+                return Err(Status::BadRequest);
+            }
+            match message_map.get("type").and_then(|type_val| type_val.as_str()) {
+                Some("url_verification") => message_map.get("challenge")
+                    .and_then(|challenge_val| challenge_val.as_str())
+                    .and_then(|challenge_str| Some(Ok(Json(json!({"challenge": challenge_str})))))
+                    .unwrap_or_else(|| Err(Status::BadRequest)),
+                Some("event_callback") => {
+                    match message_map.get("event") {
+                        Some(Value::Object(event_obj)) => handle_event_object(event_obj),
+                        _ => {
+                            info!("Got an event_callback without an event");
+                            Err(Status::BadRequest)
+                        },
+                    }
+                },
+                _ => {
+                    info!("Got invalid request, full body '{}'", serde_json::to_string(&message_map).unwrap());
+                    Err(Status::BadRequest)
+                },
+            }
         }
-        match message_map.get("type").and_then(|type_val| type_val.as_str()) {
-            Some("url_verification") => message_map.get("challenge")
-                .and_then(|challenge_val| challenge_val.as_str())
-                .and_then(|challenge_str| Some(Ok(Json(json!({"challenge": challenge_str})))))
-                .unwrap_or_else(|| Err(Status::BadRequest)),
-            Some("event_callback") => {
-                match message_map.get("event") {
-                    Some(Value::Object(event_obj)) => handle_event_object(event_obj),
-                    _ => Err(Status::BadRequest),
-                }
-            },
-            _ => Err(Status::BadRequest),
+        message_other => {
+            info!("Got invalid request, full body '{}'", serde_json::to_string(&message_other).unwrap());
+            Err(Status::BadRequest)
         }
-    }
-    else {
-        Err(Status::BadRequest)
     }
 }
 
@@ -54,14 +63,24 @@ fn handle_event_object(event: &serde_json::map::Map<String, Value>) -> Result<Js
     match event.get("type").and_then(|t| t.as_str()) {
         Some("message") => {
             match serde_json::from_value::<Message>(Value::Object(event.clone())) {
-                Err(_) => Err(Status::BadRequest),
+                Err(err) => {
+                    info!("Failed to parse message into expected struct: {}", err);
+                    Err(Status::BadRequest)
+                },
                 Ok(message) => {
                     info!("Got message from user {} on channel {} with text '{}'", message.user, message.channel, message.text);
                     Ok(Json(Value::Null))
                 },
             }
         },
-        _ => Err(Status::BadRequest)
+        Some(type_str) => {
+            info!("Got unknown event type {}", type_str);
+            Err(Status::BadRequest)
+        }
+        None => {
+            info!("Got no event type");
+            Err(Status::BadRequest)
+        }
     }
 }
 
